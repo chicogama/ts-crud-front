a309938332d87e58f962a3d8d30b9cd624d4c24b
Josemar Gama <Josemargama7@gmail.com>
2024-05-07 23:33:22 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import fastify from "fastify";
+import cors from "@fastify/cors";
+import { routes } from "./routes";
+import { error } from "console";
+import { request } from "http";
+
+const app = fastify({ logger: true });
+
+app.setErrorHandler((error, request, reply) => {
+    reply.code(400).send({ message: error.message });
+});
+
+const start = async () => {
+    await app.register(routes);
+    await app.register(cors);
+
+    try {
+        await app.listen({ port: 3333 });
+    } catch (err) {
+        process.exit(1);
+    }
+};
+
+start();
